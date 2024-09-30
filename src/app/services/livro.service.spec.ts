18d2dde0f482ad2a7768adb48b701e8b68e4fdf9
danilovalerio <danilo_vs@hotmail.com>
2024-09-30 17:15:23 -0300

    Testando a adicao de um novo livro

--- src/app/services/livro.service.spec.ts
+++ src/app/services/livro.service.spec.ts
@@ -1,3 +1,4 @@
+import { Livro } from '../componentes/livro/livro';
 import { LivroService } from './livro.service';
 
 //contexto do nosso teste
@@ -13,5 +14,23 @@ describe('LivroService', () => {
 
     //Assert
     expect(service).toBeTruthy();
+  });
+
+  it('deveria adicionar um novo livro', () => {
+    service = new LivroService();
+
+    const novoLivro: Livro = {
+      titulo: 'Novo Livro',
+      autoria: 'Autor Desconhecido',
+      imagem: 'http://example.com/cover.jpg',
+      genero: { id: 'romance', value: 'Romance' },
+      dataLeitura: '2024-04-19',
+      classificacao: 5,
+    };
+
+    service.adicionarLivro(novoLivro);
+
+    const livrosPorGenero = service.obterLivrosPorGenero('romance');
+    expect(livrosPorGenero).toContain(novoLivro);
   });
 });
