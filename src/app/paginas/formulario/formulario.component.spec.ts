f08a59f5fc7753564b0d62ffad17d03f4ac4005c
danilovalerio <danilo_vs@hotmail.com>
2024-10-02 12:53:55 -0300

    Inicializacao das variáveis e services caso tenha

--- src/app/paginas/formulario/formulario.component.spec.ts
+++ src/app/paginas/formulario/formulario.component.spec.ts
@@ -1,15 +1,22 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { FormularioComponent } from './formulario.component';
-import { ReactiveFormsModule } from '@angular/forms';
+import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
 import { RouterTestingModule } from '@angular/router/testing';
+import { LivroService } from '../../services/livro.service';
 
 describe('FormularioComponent', () => {
   let component: FormularioComponent;
   let fixture: ComponentFixture<FormularioComponent>;
+  let service: LivroService;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
       imports: [FormularioComponent, ReactiveFormsModule, RouterTestingModule],
+      providers: [[LivroService, FormBuilder]],
     });
+
+    service = TestBed.inject(LivroService);
+    fixture = TestBed.createComponent(FormularioComponent);
+    component = fixture.componentInstance;
   });
 });
